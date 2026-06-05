1b19fbb99b106772fe28d90d547b32b42758a31f
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2026-06-06 00:24:00 +0500

    setting for deploy

---
+++ api/cron.js
@@ -0,0 +1,8 @@
+const GREENLINE_ROUTE = [
+  {lat: 24.9879, lng: 67.0278, name: "Surjani"}, // Start
+  {lat: 24.9772, lng: 67.0450, name: "4K Chowrangi"},
+  {lat: 24.9610, lng: 67.0650, name: "Nazimabad"},
+  {lat: 24.9450, lng: 67.0800, name: "Board Office"},
+  {lat: 24.9260, lng: 67.0980, name: "Gulshan"},
+  {lat: 24.8950, lng: 67.0330, name: "Numaish"} // End
+]
